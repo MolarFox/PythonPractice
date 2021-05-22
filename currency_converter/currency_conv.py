6d8fe499b89b0a02c8c1a1b14c10268fb3283440
MolarFox <molar@molarfox.io>
2021-05-22 23:18:53 +1000

    Implement core features of converter

--- currency_converter/currency_conv.py
+++ currency_converter/currency_conv.py
@@ -24,23 +24,81 @@ def read_api_key(keypath):
     except FileNotFoundError:
         raise SystemExit("Error attempting to read API key file - file does not exist")
 
-def get_conv_rate():
-    pass
 
-def get_currencies(apikey):
-    response = requests.get('https://free.currconv.com/api/v7/currencies',
+def get_conv_rate(apikey, from_curr, to_curr):
+    """Gets conversion rate between currencies by using API call to service
+
+    Args:
+        apikey (str): API key for free.currconv.com
+        from_curr (str): 3 letter abbreviation of currency to convert from
+        to_curr (str): 3 letter abbreviation of currency to convert to
+
+    Returns:
+        float: conversion rate, else 0 if invalid conversion
+    """
+    # string required for API in determining conversion
+    conv_string = f"{from_curr}_{to_curr}".upper()
+    # Attempt to get conversion rate of args specified
+    conv_rate = requests.get('https://free.currconv.com/api/v7/convert',
         params={
-        'apiKey':apikey
+            'apiKey':apikey,
+            'compact':'ultra',
+            'q':conv_string
         }
     )
 
-    raw_vals = json.loads(response.text)["results"]
-    return raw_vals
+    # Return 0 if the conversion rate was invalid, else parse and return float
+    if conv_rate.text == "{}":
+        return 0
+    else:
+        return float(json.loads(conv_rate.text)[conv_string])
+    
+
+def get_currencies(apikey):
+    """Gets dictionary containing details of all possible currencies
+            Has currency full name, 3-letter abbreviation, and symbol (where available)
+
+    Args:
+        apikey (str): API key for free.currconv.com
+
+    Returns:
+        {str: {str:str}}: Dictionary containing parsed json response from API
+    """
+    response = requests.get('https://free.currconv.com/api/v7/currencies',
+        params={
+            'apiKey':apikey
+        }
+    )
+
+    # Parse and return the response
+    return json.loads(response.text)["results"]
+
+
+def convert(apikey, from_curr, to_curr, amount=1.0):
+    """Converts amount of a currency to another currency
+
+    Args:
+        apikey (str): API key for free.currconv.com
+        from_curr (str): 3 letter abbreviation of currency to convert from
+        to_curr (str): 3 letter abbreviation of currency to convert to
+        amount (float, optional): Amount of currency to convert. Defaults to 1.
+
+    Returns:
+        float: Amount of output currency representing same value as amount of input currency
+    """
+    conv_rate = get_conv_rate(apikey, from_curr, to_curr)
+    return amount * conv_rate
+
+
 
 if __name__ == '__main__':
     apikey = read_api_key("apikey.txt")
 
-    print("Welcome to a cool and good cli currency converter by MolarFox\n")
+    print("\nWelcome to a cool and good cli currency converter by MolarFox\n")
+
+    print(get_conv_rate(apikey, "USD", "AUD"))
+    print(get_conv_rate(apikey, "USD", "aud"))
+    print(get_conv_rate(apikey, "USD", "shittiddies"))
 
     # Define dictionary to act as pseudo- switch case
     cli_handler = {
